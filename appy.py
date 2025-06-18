ad00caaae1420b23adcadf2ec84b20d661b6cf39
Andre-StudioNerd <deco.luisfernandes@gmail.com>
2025-06-18 13:48:24 -0300

    refatorando ifs

--- appy.py
+++ appy.py
@@ -1,8 +1,8 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
+restaurante_praca.receber_avaliacao('Gui', 1)
+restaurante_praca.receber_avaliacao('Lais', 2)
 restaurante_praca.receber_avaliacao('Emy', 5)
 
 
